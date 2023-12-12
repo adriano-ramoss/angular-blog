a9932b34960859b61c6137840e22077b2a8bda42
adriano-ramoss <adrianoramos2401@gmail.com>
2023-12-12 15:16:55 -0300

    Subindo Arquivos

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MenuTitleComponent } from '../../component/menu-title/menu-title.component';
+import { BigCardComponent } from '../../component/big-card/big-card.component';
+import { SmallCardComponent } from '../../component/small-card/small-card.component';
+import { MenuBarComponent } from '../../component/menu-bar/menu-bar.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent, MenuBarComponent  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
